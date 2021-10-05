f03f6309a613ae20009578af4cee8d66f004387c
zjt_work <33414218+jiantaiz@users.noreply.github.com>
2021-10-05 15:59:39 +0800

    可以在实验室主机运行

--- bin/nvccbat.bat
+++ bin/nvccbat.bat
@@ -1,5 +1,5 @@
 echo off
-set PATH=C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\bin\amd64\;%PATH%
-set PATH=D:\CUDA\NVIDIA GPU Computing Toolkit\v11.0\bin;%PATH%
+set PATH=C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Tools\MSVC\14.28.29333\bin\Hostx64\x64\;%PATH%
+set PATH=C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v10.2\bin;%PATH%
 call vcvarsamd64.bat
 nvcc %1 %2 %3 %4 %5 %6 %7 %8 %9 
